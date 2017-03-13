37c4ab0aa00d0c3c535b4d7e4b44612851fbb946
fran-Lee <lijian2010220@163.com>
2017-03-13 20:59:01 +0800

    增加后台自动更新天气的功能

--- src/com/honeyweather/app/activity/WeatherActivity.java
+++ src/com/honeyweather/app/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.honeyweather.app.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -13,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.honeyweather.app.R;
+import com.honeyweather.app.service.AutoUpdateService;
 import com.honeyweather.app.util.HttpCallbackListener;
 import com.honeyweather.app.util.HttpUtil;
 import com.honeyweather.app.util.Utility;
@@ -85,6 +87,9 @@ public class WeatherActivity extends Activity implements OnClickListener{
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		
+		Intent intent = new Intent(this, AutoUpdateService.class);
+		startService(intent);
 	}
 
 	private void queryWeatherCode(String countyCode) {
@@ -144,7 +149,24 @@ public class WeatherActivity extends Activity implements OnClickListener{
 
 	@Override
 	public void onClick(View v) {
-		// TODO Auto-generated method stub
+		switch (v.getId()){
+		case R.id.switch_city:
+			Intent intent = new Intent(this, ChooseAreaActivity.class);
+			intent.putExtra("from_weather_activity", true);
+			startActivity(intent);
+			finish();
+			break;
+		case R.id.refresh_weather:
+			publishText.setText("Í¬²½ÖÐ...");
+			SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+			String weatherCode = prefs.getString("weather_code", "");
+			if(!TextUtils.isEmpty(weatherCode)){
+				queryWeatherInfo(weatherCode);
+			}
+			break;
+		default:
+			break;
+		}
 		
 	}
 
---
+++ src/com/honeyweather/app/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,17 @@
+package com.honeyweather.app.receiver;
+
+import com.honeyweather.app.service.AutoUpdateService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class AutoUpdateReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent i = new Intent(context, AutoUpdateService.class);
+		context.startService(i);
+	}
+
+}
